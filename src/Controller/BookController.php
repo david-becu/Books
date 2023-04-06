3389deaed1b9c4c6c1bc31295ab44aa9377d59ab
Macnimo <david.macnimo@gmail.com>
2023-04-06 17:51:08 +0200

    getAllBooks + getDetailBook

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -13,29 +13,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class BookController extends AbstractController
 {
     #[Route('/api/books/', name:'books', methods:['GET'])]
-function getBookList(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
+function getAllBooks(BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
     {
     $bookList = $bookRepository->findAll();
-    $jsonBookList = $serializer->serialize($bookList, 'json');
-    return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
+    return $this->json($bookList, $status = 200, $headers = [], ['groups' => 'getBooks']);git add
+    // $jsonBookList = $serializer->serialize($bookList, 'json', ['groups'=>'getBooks']);
+    // return new JsonResponse($jsonBookList, Response::HTTP_OK, [], true);
 }
 
-// #[Route('/api/books/{id}', name:'detailBook', methods:['GET'])]
-// function getDetailBook(int $id, BookRepository $bookRepository, SerializerInterface $serializer): JsonResponse
-//     {
-//     $book = $bookRepository->find($id);
-//     if ($book) {
-//         $jsonBook = $serializer->serialize($book, 'json');
-//         return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
-//     }
-//     return new JsonResponse(null, Response::HTTP_NOT_FOUND);
-// }
-
 #[Route('/api/books/{id}', name:'detailBook', methods:['GET'])]
-function getDetailBook(Book $book, SerializerInterface $serializer): JsonResponse
+function getDetailBook(int $id, SerializerInterface $serializer, BookRepository $bookRepository): JsonResponse
     {
-    $jsonBook = $serializer->serialize($book, 'json');
-    return new JsonResponse($jsonBook, Response::HTTP_OK, ['accept' => 'json'], true);
+    $book = $bookRepository->find($id);
+    if ($book) {
+        return $this->json($book, $status = 200, $headers = [], ['groups' => 'getBooks']);
+        // $jsonBook = $serializer->serialize($book, 'json', ['groups'=>'getBooks']);
+        // return new JsonResponse($jsonBook, Response::HTTP_OK, [], true);
+    }
+    return new JsonResponse(null, Response::HTTP_NOT_FOUND);
 }
 
 }
