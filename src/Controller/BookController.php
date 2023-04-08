16a2c9a4554b708e2d9f2f4b69de5e567d044121
Macnimo <david.macnimo@gmail.com>
2023-04-08 11:56:35 +0200

    add createBook is ok

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Book;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookController extends AbstractController {
@@ -39,6 +41,29 @@ class BookController extends AbstractController {
         $managerBook->remove( $book );
         $managerBook->flush();
         return new JsonResponse( null, Response::HTTP_NO_CONTENT );
+    }
+
+    #[Route('/api/books', name:"createBook", methods: ['POST'])]
+    public function createBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $managerBook, UrlGeneratorInterface $urlGenerator): JsonResponse {
+        
+        // Récupération du contenu de la requête sous la forme d'un objet Book enregistré dans la variable $book
+        $book = $serializer->deserialize( $request->getContent(), Book::class, 'json');
+        
+        // Enregistrement du livre
+        $managerBook->persist($book);
+        
+        // Confirmation de l'enregistrement
+        $managerBook->flush();
+        
+        // jsonBook est retournée pour information à l'envoyeur
+        $jsonBook = $serializer->serialize( $book, 'json', [ 'groups'=>'getBooks' ] );
+        
+        // l'usage demande dans le cadre du POST d'ajouter une information dans le HEADER de la réponse.
+        // $location permet d'indiquer le chemin de l'URL de la requête et sera disponible pour 
+        // information afin de tester que l'URL a réellement été créée 
+        $location = $urlGenerator->generate('detailBook', ['id'=>$book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
 }
