b3f7d5fb5e64ef3d29bce10e1780f0201d7228fd
Macnimo <david.macnimo@gmail.com>
2023-04-08 16:18:35 +0200

    add updateBook

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Book;
 use App\Repository\BookRepository;
+use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,11 +12,11 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookController extends AbstractController {
     #[ Route( '/api/books/', name:'books', methods:[ 'GET' ] ) ]
-
     function getAllBooks( BookRepository $bookRepository, SerializerInterface $serializer ): JsonResponse {
         $bookList = $bookRepository->findAll();
         return $this->json( $bookList, $status = 200, $headers = [], [ 'groups' => 'getBooks' ] );
@@ -24,7 +25,6 @@ class BookController extends AbstractController {
     }
 
     #[ Route( '/api/books/{id}', name:'detailBook', methods:[ 'GET' ] ) ]
-
     function getDetailBook( int $id, SerializerInterface $serializer, BookRepository $bookRepository ): JsonResponse {
         $book = $bookRepository->find( $id );
         if ( $book ) {
@@ -36,34 +36,59 @@ class BookController extends AbstractController {
     }
 
     #[ Route( '/api/books/{id}', name: 'deleteBook', methods: [ 'DELETE' ] ) ]
-
     public function deleteBook( Book $book, EntityManagerInterface $managerBook ): JsonResponse {
+        
         $managerBook->remove( $book );
         $managerBook->flush();
         return new JsonResponse( null, Response::HTTP_NO_CONTENT );
     }
 
-    #[Route('/api/books', name:"createBook", methods: ['POST'])]
-    public function createBook(Request $request, SerializerInterface $serializer, EntityManagerInterface $managerBook, UrlGeneratorInterface $urlGenerator): JsonResponse {
-        
+    #[ Route( '/api/books', name:'createBook', methods: [ 'POST' ] ) ]
+    public function createBook( Request $request, SerializerInterface $serializer, EntityManagerInterface $managerBook, UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository ): JsonResponse {
+
         // Récupération du contenu de la requête sous la forme d'un objet Book enregistré dans la variable $book
         $book = $serializer->deserialize( $request->getContent(), Book::class, 'json');
         
+        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        $content = $request->toArray();
+
+        // Récupération de l'auteur. S'il n'est pas défini, alors on met -1 par défaut.
+        $idAuthor = $content['idAuthor'] ?? -1;
+
+        // On cherche l'auteur qui correspond et on l'assigne au livre.
+        // Si "find" ne trouve pas l'auteur, alors null sera retourné.
+        $book->setAuthor( $authorRepository->find( $idAuthor ) );
         // Enregistrement du livre
-        $managerBook->persist($book);
-        
+        $managerBook->persist( $book );
+
         // Confirmation de l'enregistrement
         $managerBook->flush();
         
         // jsonBook est retournée pour information à l'envoyeur
         $jsonBook = $serializer->serialize( $book, 'json', [ 'groups'=>'getBooks' ] );
-        
-        // l'usage demande dans le cadre du POST d'ajouter une information dans le HEADER de la réponse.
-        // $location permet d'indiquer le chemin de l'URL de la requête et sera disponible pour 
-        // information afin de tester que l'URL a réellement été créée 
-        $location = $urlGenerator->generate('detailBook', ['id'=>$book->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
-        return new JsonResponse($jsonBook, Response::HTTP_CREATED, ["Location" => $location], true);
+        // l'usage demande dans le cadre du POST d'ajouter une information dans le HEADER de la réponse.
+        // $location permet d'indiquer le chemin de l'URL de la requête et sera disponible pour
+        // information afin de tester que l'URL a réellement été créée 
+        $location = $urlGenerator->generate('detailBook', ['id'=>$book->getId() ], UrlGeneratorInterface::ABSOLUTE_URL );
+
+        return new JsonResponse( $jsonBook, Response::HTTP_CREATED, [ 'Location' => $location ], true );
+    }
+
+    #[ Route( '/api/books/{id}', name:'updateBook', methods: [ 'PUT' ] ) ]
+    public function updateBook( Request $request, SerializerInterface $serializer, Book $currentBook, EntityManagerInterface $managerBook, UrlGeneratorInterface $urlGenerator, AuthorRepository $authorRepository ): JsonResponse {
+        
+        $updatedBook = $serializer->deserialize($request->getContent(), Book::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE=>$currentBook]);
+        
+        $content = $request->toArray();
+        $idAuthor = $content['idAuthor'] ?? -1;
+
+        $updatedBook->setAuthor($authorRepository->find($idAuthor));
+
+        $managerBook->persist($updatedBook);
+        $managerBook->flush();
+
+        return new JsonResponse( null, JsonResponse::HTTP_NO_CONTENT);
     }
 
 }
